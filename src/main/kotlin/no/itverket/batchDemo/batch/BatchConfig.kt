6f5cbd481651cdfedddcd1ecd8710d99d1e3d495
Eirik Tveterås <eirik.tveteraas@gmail.com>
2023-04-30 10:32:41 +0200

    Basic batch configuration

--- src/main/kotlin/no/itverket/batchDemo/batch/BatchConfig.kt
+++ src/main/kotlin/no/itverket/batchDemo/batch/BatchConfig.kt
@@ -1,6 +1,10 @@
 package no.itverket.batchDemo.batch
 
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing
+import org.springframework.batch.core.explore.JobExplorer
+import org.springframework.batch.core.launch.JobLauncher
+import org.springframework.batch.core.repository.JobRepository
+import org.springframework.boot.autoconfigure.batch.JobLauncherApplicationRunner
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder
@@ -13,6 +17,10 @@ import javax.sql.DataSource
  *
  * Konfigurert deklarativt med @EnableBatchProcessing. Alternativt kan man extende DefaultBatchConfiguration og
  * override de metodene man ønsker, f.eks getDataSource().
+ *
+ * Notat: Med spring batch 5 (Spring Boot 3) så kan man droppe både EnableBatchConfiguration og
+ * DefaultBatchConfiguration. Spring vil selv skjønne at dette er en batch-applikasjon, og vil autokonfigureres
+ * deretter.
  */
 @Configuration
 @EnableBatchProcessing(dataSourceRef = "batchDataSource", transactionManagerRef = "batchTransactionManager")
@@ -27,4 +35,14 @@ class BatchConfig {
 
     @Bean
     fun batchTransactionManager(): JdbcTransactionManager = JdbcTransactionManager(batchDataSource())
+
+    /**
+     * Automatisk oppstart av jobben når applikasjonen kjøres.
+     */
+    @Bean
+    fun jobLauncherApplicationRunner(
+        jobLauncher: JobLauncher,
+        jobExplorer: JobExplorer,
+        jobRepository: JobRepository
+    ): JobLauncherApplicationRunner = JobLauncherApplicationRunner(jobLauncher, jobExplorer, jobRepository)
 }
